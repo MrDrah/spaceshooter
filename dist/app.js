9a9154732a9d6cea53be19d5e033970c68594b4e
MrDrah <q.milavec@gmail.com>
2025-02-06 12:17:24 +0900

    Innitial commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+import Screen from "./class/screen.js";
+$("body").css("color", "red");
+console.log(Screen.width);
+console.log(Screen.height);
---
+++ src/app.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+import  Screen  from "./class/screen.js";
+
+
+$("body").css("color", "red");
+
+console.log(Screen.width);
+console.log(Screen.height);
+
